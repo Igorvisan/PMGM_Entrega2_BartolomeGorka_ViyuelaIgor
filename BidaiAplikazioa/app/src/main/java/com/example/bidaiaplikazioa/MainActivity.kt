6a822eb52fcbd81c80651828113ed80e10767863
igorvisan <igo_viy_san@goierrieskola.org>
2024-10-24 20:11:55 +0200

    moverse entre pantalla de inicio y principal añadido

--- BidaiAplikazioa/app/src/main/java/com/example/bidaiaplikazioa/MainActivity.kt
+++ BidaiAplikazioa/app/src/main/java/com/example/bidaiaplikazioa/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.IntegerRes
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,13 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,11 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.bidaiaplikazioa.R.color.black
 import com.example.bidaiaplikazioa.ui.theme.BidaiAplikazioaTheme
 
 
@@ -42,15 +34,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BidaiAplikazioaTheme {
-                myAppPreviewr()
+                ShowApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
     }
 
 @Composable
-fun pantallaIniciarSesion(modifier: Modifier = Modifier){
-    var mostrarAlPrincipio by remember { mutableStateOf(true) }
+fun pantallaIniciarSesion(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
     var text by remember { mutableStateOf("") }
     Surface(
         modifier,
@@ -74,7 +68,7 @@ fun pantallaIniciarSesion(modifier: Modifier = Modifier){
                 modifier = Modifier.padding(vertical = 18.dp),
             )
             Button(
-                onClick = {mostrarAlPrincipio = false},
+                onClick = onContinueClicked,
                 modifier.padding(vertical = 24.dp),
                 colors = ButtonDefaults.buttonColors(colorResource(id = R.color.black)),
             ) {
@@ -86,12 +80,42 @@ fun pantallaIniciarSesion(modifier: Modifier = Modifier){
 
 @Composable
 fun paginaPrincipal(modifier: Modifier = Modifier){
-    
+    Surface (
+        modifier,
+        color = colorResource(id = R.color.cream)
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize()
+        )
+        {
+            Text("Hello")
+        }
+
+    }
+}
+
+@Composable
+fun ShowApp(modifier: Modifier = Modifier){
+    var mostrarAlPrincipio by remember { mutableStateOf(true) }
+
+    Surface(modifier){
+        if(mostrarAlPrincipio){
+            pantallaIniciarSesion(onContinueClicked = {mostrarAlPrincipio = false})
+        }else{
+            paginaPrincipal()
+        }
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
 fun myAppPreviewr(){
-    pantallaIniciarSesion()
+    pantallaIniciarSesion(onContinueClicked = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun previewPantallaPrincipak(){
+    paginaPrincipal()
 }
