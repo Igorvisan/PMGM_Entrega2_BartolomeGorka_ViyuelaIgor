4ac1dd2c1b8dc66de037de38ff852a050c1c4605
igorvisan <igo_viy_san@goierrieskola.org>
2024-10-24 19:42:18 +0200

    pantalla de inicio creada

--- BidaiAplikazioa/app/src/main/java/com/example/bidaiaplikazioa/MainActivity.kt
+++ BidaiAplikazioa/app/src/main/java/com/example/bidaiaplikazioa/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.IntegerRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,9 +27,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.bidaiaplikazioa.R.color.black
 import com.example.bidaiaplikazioa.ui.theme.BidaiAplikazioaTheme
+
 
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +42,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BidaiAplikazioaTheme {
+                myAppPreviewr()
                 }
             }
         }
@@ -38,41 +51,46 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun pantallaIniciarSesion(modifier: Modifier = Modifier){
     var mostrarAlPrincipio by remember { mutableStateOf(true) }
-    Surface(modifier){
+    var text by remember { mutableStateOf("") }
+    Surface(
+        modifier,
+        color = colorResource(id = R.color.cream)
+    ){
         Column (
             modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ){
-            Text("Bienvenido a nuestra aplicación de viajes")
+            Text(
+                "Welcome to our booking application!"
+            )
+            OutlinedTextField(
+                value = text,
+                onValueChange = {text = it}, //onValueChange = { text = it }: Cada vez que el usuario escriba algo,
+                // el contenido del campo de texto cambiará y actualizará el valor de text
+                label = { Text("Name") },
+                placeholder = {Text("Enter Name")},
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.padding(vertical = 18.dp),
+            )
             Button(
                 onClick = {mostrarAlPrincipio = false},
-                modifier.padding(vertical = 24.dp)
+                modifier.padding(vertical = 24.dp),
+                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.black)),
             ) {
-                Text("Iniciar Sesion")
+                Text("Enter")
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun paginaPrincipal(modifier: Modifier = Modifier){
+    
 }
+
 
 @Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BidaiAplikazioaTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview
 @Composable
 fun myAppPreviewr(){
     pantallaIniciarSesion()
